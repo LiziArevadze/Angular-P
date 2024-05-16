bb573aacc5709cfc8aa6959abbe19bbeedb5626b
LiziArevadze <151961473+LiziArevadze@users.noreply.github.com>
2024-05-16 23:59:41 +0400

    Update cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.services';
+import { Product } from '../home/home.component';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: Product[] = [];
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCartItems();
+    console.log('Cart Items in CartComponent:', this.cartItems);
+  }
+
+  removeItem(product: Product): void {
+    this.cartService.removeFromCart(product);
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price, 0);
+  }
 }
