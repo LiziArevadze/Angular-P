f7c4acb7e29034a9bdd98f51a058ab6929b396fc
LiziArevadze <151961473+LiziArevadze@users.noreply.github.com>
2024-05-16 23:56:41 +0400

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { CartService } from '../services/cart.services';
 
 export class Product {
-  public hovered: boolean = false;
   constructor(
     public name: string,
-    public imageUrl: string,
     public price: number
   ) {}
 }
@@ -15,25 +13,22 @@ export class Product {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   products: Product[] = [
-    new Product('UI Design for beginners', '\assets\Placeholder 3.jpg', 24.92),
-    new Product('VUE Javascript Course', '\assets\Placeholder 4.jpg', 24.92),
-    new Product('Mobile DEV REACT Native', '\assets\Placeholder 5.jpg', 24.92),
-    new Product('Website DEV Hero to Zero', '\assets\Placeholder 6.jpg', 24.92),
-    new Product('Learn Programming in 30 days', '\assets\Placeholder 7.jpg', 24.92)
+    new Product('UI Design for beginners', 24.92),
+    new Product('VUE Javascript Course', 24.92),
+    new Product('Mobile DEV REACT Native', 24.92),
+    new Product('Website DEV Hero to Zero', 24.92),
+    new Product('Learn Programming in 30 days', 24.92)
   ];
 
   message: string = '';
      
   constructor(private cartService: CartService) { }
 
-  addToCart(product: Product): void {
+  addToCart(product: Product) {
     this.cartService.addToCart(product);
     this.message = 'Product added to cart: ' + product.name;
-  }
-
-  handleClick(product: Product): void {
-    this.addToCart(product);
   }
 }
