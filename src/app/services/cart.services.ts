0781e9eceb62ec30aa4918a224be39f4bf45ce7a
LiziArevadze <151961473+LiziArevadze@users.noreply.github.com>
2024-05-16 23:48:50 +0400

    Update cart.services.ts

--- src/app/services/cart.services.ts
+++ src/app/services/cart.services.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../home/home.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServicesService {
+export class CartService {
+  cartItems: Product[] = [];
 
   constructor() { }
+
+  addToCart(product: Product): void {
+    this.cartItems.push(product);
+    console.log('Cart Items after adding:', this.cartItems);
+  }
+  
+
+  getCartItems(): Product[] {
+  return this.cartItems;
+  }
+
+  removeFromCart(product: Product): void {
+    const index = this.cartItems.findIndex(item => item === product);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+      console.log('Item removed from cart:', product);
+    }
+  }
+
+  clearCart(): void {
+    this.cartItems = [];
+    console.log('Cart cleared');
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price, 0);
+  }
 }
